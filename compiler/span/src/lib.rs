19df95537530080839180f5d5f08afa55e67edb7
Lictor Guzman <lctrgzmn@gmail.com>
2022-04-24 20:58:18 -0500

    added union methods to Span and Location, as well as other potentially useful auxilliary methods

---
+++ compiler/span/src/lib.rs
@@ -0,0 +1,446 @@
+use std::ops::{Add, AddAssign};
+
+use wy_common::newtype;
+
+newtype! {
+    { u32 in Row | Show Usize Deref (+=) (-) }
+    { u32 in Col | Show Usize Deref (+=) (-) }
+    { u32 in Pos | Show Usize Deref (+=) (-) }
+}
+
+impl Add<char> for Pos {
+    type Output = Self;
+
+    fn add(self, rhs: char) -> Self::Output {
+        Pos(self.0 + rhs.len_utf8() as u32)
+    }
+}
+
+impl AddAssign<char> for Pos {
+    fn add_assign(&mut self, rhs: char) {
+        self.0 += rhs.len_utf8() as u32
+    }
+}
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Span(pub Pos, pub Pos);
+
+#[derive(Clone, Copy, Debug, Hash)]
+pub struct Spanned<T>(pub T, pub Span);
+
+pub trait Dummy
+where
+    Self: Sized + PartialEq,
+{
+    const DUMMY: Self;
+    fn dummy() -> Self {
+        Self::DUMMY
+    }
+    fn is_dummy(&self) -> bool {
+        self == &Self::DUMMY
+    }
+    fn partial_dummy(&self) -> bool;
+}
+
+impl Dummy for Row {
+    const DUMMY: Self = Self::ZERO;
+
+    fn partial_dummy(&self) -> bool {
+        self.is_zero()
+    }
+}
+
+impl Dummy for Col {
+    const DUMMY: Self = Self::MAX;
+
+    fn partial_dummy(&self) -> bool {
+        self.is_dummy()
+    }
+}
+
+pub trait WithSpan {
+    fn get_pos(&self) -> Pos;
+
+    fn as_index(&self) -> usize {
+        self.get_pos().as_usize()
+    }
+
+    fn spanned<F, X>(&mut self, mut f: F) -> Spanned<X>
+    where
+        F: FnMut(&mut Self) -> X,
+    {
+        let start = self.get_pos();
+        let ret = f(self);
+        let end = self.get_pos();
+        Spanned(ret, Span(start, end))
+    }
+}
+
+pub trait Layout {
+    fn get_loc(&self) -> Loc;
+    fn get_row(&self) -> Row {
+        Layout::get_loc(self).row
+    }
+    fn get_col(&self) -> Col {
+        Layout::get_loc(self).col
+    }
+}
+
+impl Pos {
+    pub fn dummy() -> Pos {
+        Pos::ZERO
+    }
+
+    pub fn is_dummy(&self) -> bool {
+        self.is_zero()
+    }
+
+    /// Returns the number of bytes spanned by a UTF8 encoded character.
+    pub fn len_utf8(c: char) -> Self {
+        Self(c.len_utf8() as u32)
+    }
+
+    pub fn as_u32(self) -> u32 {
+        *self
+    }
+
+    pub fn strlen<S: AsRef<str>>(string: S) -> Self {
+        Self(string.as_ref().len() as u32)
+    }
+}
+
+impl WithSpan for Pos {
+    fn get_pos(&self) -> Pos {
+        *self
+    }
+}
+
+impl std::fmt::Display for Span {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.0, self.1)
+    }
+}
+
+impl Span {
+    pub fn new(start: impl Into<Pos>, end: impl Into<Pos>) -> Self {
+        Self(start.into(), end.into())
+    }
+
+    pub fn tuple(self) -> (Pos, Pos) {
+        (self.0, self.1)
+    }
+
+    pub fn len(&self) -> usize {
+        (self.1 - self.0).as_usize()
+    }
+
+    pub fn start(&self) -> Pos {
+        self.0
+    }
+
+    pub fn end(&self) -> Pos {
+        self.1
+    }
+
+    pub fn begin<X: WithSpan>(start: Pos) -> impl FnMut(X) -> Span {
+        move |x: X| Span(start, x.get_pos())
+    }
+
+    pub fn range(&self) -> std::ops::Range<usize> {
+        (self.start().as_usize())..(self.end().as_usize())
+    }
+
+    pub fn union(&self, other: Span) -> Self {
+        let mut poss = [self.0, self.1, other.0, other.1];
+        poss.sort_unstable();
+        Span(poss[0], poss[3])
+    }
+}
+
+impl Dummy for Span {
+    const DUMMY: Self = Self(Pos::ZERO, Pos::ZERO);
+
+    fn is_dummy(&self) -> bool {
+        self.0.is_dummy() && self.1.is_dummy() || (self.0 >= self.1)
+    }
+
+    fn partial_dummy(&self) -> bool {
+        self.0.is_dummy() || self.1.is_dummy()
+    }
+}
+
+impl<T> Spanned<T> {
+    pub fn new(item: T, span: Span) -> Self {
+        Self(item, span)
+    }
+
+    pub fn join<U>(self, rhs: Spanned<U>) -> Spanned<(T, U)> {
+        Spanned((self.0, rhs.0), Span(self.1 .0, rhs.1 .1))
+    }
+
+    pub fn as_tuple(self) -> (T, Span) {
+        (self.0, self.1)
+    }
+
+    #[inline]
+    pub fn item(&self) -> &T {
+        &self.0
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+
+    #[inline]
+    pub fn span(&self) -> Span {
+        self.1
+    }
+
+    pub fn map<U, F: FnMut(T) -> U>(self, mut f: F) -> Spanned<U> {
+        Spanned(f(self.0), self.1)
+    }
+
+    /// Given a span, returns a new span containing the minimum `Pos` as the
+    /// starting point and the maximum `Pos` as the ending point.
+    pub fn union(&self, other: &Span) -> Self
+    where
+        T: Clone,
+    {
+        let Spanned(t, Span(a1, a2)) = self.clone();
+        let Span(b1, b2) = *other;
+        let mut ss = [a1, a2, b1, b2];
+        ss.sort();
+        Spanned(t, Span(ss[0], ss[3]))
+    }
+}
+
+impl<T> Eq for Spanned<T> where T: Eq {}
+
+impl<T> std::cmp::PartialEq for Spanned<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.span() == other.span()
+    }
+}
+
+impl<T> std::cmp::PartialOrd for Spanned<T>
+where
+    T: PartialEq,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.1.partial_cmp(&other.1)
+    }
+}
+
+impl Layout for Loc {
+    fn get_loc(&self) -> Loc {
+        *self
+    }
+}
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Loc {
+    pub row: Row,
+    pub col: Col,
+}
+
+impl Default for Loc {
+    fn default() -> Self {
+        Self {
+            row: Row::ONE,
+            col: Col::ZERO,
+        }
+    }
+}
+
+impl Dummy for Loc {
+    /// Dummy `Position` for situations in which the only requirement
+    /// is the existence of a `Position`. Useful as an intermediate value.
+    ///
+    /// Note: dummy values can be identified by having a `row` value
+    /// of `Row::ZERO` or a `col` value of `u32::MAX`, while `Position`
+    /// has a default (and starting) `row` value of `Row::ONE`.
+    const DUMMY: Self = Self {
+        row: Row::ZERO,
+        col: Col::MAX,
+    };
+
+    fn partial_dummy(&self) -> bool {
+        self.row.is_dummy() || self.col.is_dummy()
+    }
+}
+
+impl Loc {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Increments the `col` field by 1 and, for the char given,
+    /// returns a `BytePos` containing the number of bytes in the utf8
+    /// encoding of that character.
+    pub fn incr_column(&mut self, ch: char) -> Pos {
+        let len = ch.len_utf8() as u32;
+        self.col += Col::ONE;
+        Pos(len)
+    }
+
+    /// Resets the column value in the `col` to 0. This occurs when
+    /// encountering a line-feed character `\n` during lexing.
+    pub fn reset_col(&mut self) {
+        self.col = Col::ZERO;
+    }
+
+    /// Increments the `row` fields by 1, resetting the `col`
+    /// field to `0`. Like with `incr_column`, this returns the number
+    /// of bytes read.
+    pub fn incr_row(&mut self) -> Pos {
+        // self.pos += 1;
+        self.row += Row::ONE;
+        self.reset_col();
+        Pos('\n'.len_utf8() as u32)
+    }
+
+    /// Since we always begin on a `row` value of 1, we can easily tell whether
+    /// a given `Span` is invalid based on whether `row` has a 0 value.
+    ///
+    /// Additionally, a `dummy` column value is defined as the highest column
+    /// value attainable by the `Col`'s inner type, i.e., `u32::MAX`.
+    pub fn is_dummy(&self) -> bool {
+        self.row.is_dummy() || self.col.is_dummy()
+    }
+
+    /// Utility method for comparing `Location` structs
+    pub fn row_eq(&self, rhs: &Self) -> bool {
+        self.row == rhs.row
+    }
+
+    pub fn col_eq(&self, rhs: &Self) -> bool {
+        self.col == rhs.col
+    }
+
+    /// First, we say a [`Loc`] `x` *contains* a [`Loc`] `y` if
+    ///
+    ///     x.row == y.row || (x.row < y.row && x.col < y.col)
+    ///
+    /// i.e, any of the following hold:
+    ///
+    /// * `y` is on the same row (= line) as `x`
+    /// * both `row` and `col` values of `y` are greater than that of `x`
+    pub fn contains(&self, rhs: &Self) -> bool {
+        self.row == rhs.row || (self.row < rhs.row && self.col < rhs.col)
+    }
+
+    pub fn rows_between(&self, rhs: &Self) -> u32 {
+        self.row.abs_diff(rhs.row.0)
+    }
+
+    pub fn cols_between(&self, rhs: &Self) -> u32 {
+        self.col.abs_diff(rhs.col.0)
+    }
+}
+
+impl std::fmt::Display for Loc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_dummy() {
+            write!(f, "(?:?)")
+        } else {
+            write!(f, "{}:{}", self.row, self.col)
+        }
+    }
+}
+
+impl<'t> std::ops::Index<Row> for std::str::Lines<'t> {
+    type Output = str;
+
+    fn index(&self, index: Row) -> &Self::Output {
+        let row = index.as_usize();
+        for (r, s) in self.clone().enumerate() {
+            if r == row {
+                return s;
+            }
+        }
+        ""
+    }
+}
+
+/// Essentially a `Span`, but instead of holding byte positions, it holds `Loc`
+/// values to form a "rectangular" range, where the `start` corresponds to the
+/// top left corner, and the `end` corresponds to the bottom right. Note that
+/// the `row` values of the contained `Loc` values may coincide, effectively
+/// describing a line (with the same analogously applying to respective `col`
+/// values).
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Location {
+    pub start: Loc,
+    pub end: Loc,
+}
+
+impl Location {
+    /// Given another `Location`, will return a new `Location` such that the
+    /// starting `Loc` contains the minimum `row` and `col` values, and the
+    /// ending `Loc` contains the maximum `row` and `col` values.
+    pub fn union(&self, other: &Self) -> Self {
+        let mut rows =
+            [self.start.row, self.end.row, other.start.row, other.end.row];
+        let mut cols =
+            [self.start.col, self.end.col, other.start.col, other.end.col];
+        rows.sort();
+        cols.sort();
+        Location {
+            start: Loc {
+                row: rows[0],
+                col: cols[0],
+            },
+            end: Loc {
+                row: rows[3],
+                col: cols[3],
+            },
+        }
+    }
+
+    /// Returns the number of rows between the starting `Loc` and the ending
+    /// `Loc`. Note: this does NOT take sign into account.
+    pub fn rows(&self) -> u32 {
+        self.start.row.abs_diff(self.end.row.0)
+    }
+
+    /// Returns the number of columns between the starting `Loc` and the ending
+    /// `Loc`. Note: this does NOT take sign into account.
+    pub fn cols(&self) -> u32 {
+        self.start.col.abs_diff(self.end.col.0)
+    }
+}
+
+impl Default for Location {
+    fn default() -> Self {
+        Self {
+            start: Loc::default(),
+            end: Loc::default(),
+        }
+    }
+}
+
+impl Dummy for Location {
+    const DUMMY: Self = Self {
+        start: Loc::DUMMY,
+        end: Loc::DUMMY,
+    };
+
+    fn partial_dummy(&self) -> bool {
+        self.start.is_dummy()
+            || self.end.is_dummy()
+            || self.start.row > self.end.row
+            || self.start.col > self.end.col
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
