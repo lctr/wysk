726200e28983f56d30603b60e1bdea1f103899e8
Lictor Guzman <lctrgzmn@gmail.com>
2022-05-18 18:13:11 -0500

    addded primitive/builtin type name symbols

--- compiler/intern/src/lib.rs
+++ compiler/intern/src/lib.rs
@@ -1,3 +1,21 @@
 pub mod symbol;
 
 pub use symbol::*;
+
+pub mod sym {
+    use super::{intern_many, Symbol};
+
+    lazy_static::lazy_static! {
+        pub static ref PRIM_TY_NAMES: [Symbol; 9] = intern_many([
+            "Bool",
+            "Byte",
+            "Int",
+            "Nat",
+            "Float",
+            "Double",
+            "Char",
+            "Str",
+            "IO"
+        ]);
+    }
+}
