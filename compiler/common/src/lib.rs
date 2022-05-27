aa5a8aafd2a9b14f99b58c0226a1b9a6841b71e8
Lictor Guzman <lctrgzmn@gmail.com>
2022-05-26 19:48:38 -0500

    added some getter-like method generating macros for structs

--- compiler/common/src/lib.rs
+++ compiler/common/src/lib.rs
@@ -70,6 +70,61 @@ macro_rules! deque {
     }};
 }
 
+#[macro_export]
+macro_rules! struct_field_iters {
+    (
+        $(|$g0:ident $(, $gs:ident)*|)?
+        $object:ty
+        $(
+           | $field:ident $(. $rest:ident)* => $method_name:ident :: $ty:ty
+        )+
+    ) => {
+        impl $(<$g0 $(, $gs)*>)? $object {
+            $(
+                #[inline]
+                pub fn $method_name(&self) -> std::slice::Iter<'_, $ty> {
+                    self.$field $(.$rest)* .iter()
+                }
+            )+
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! struct_getters {
+    (
+        $(|$g0:ident $(, $gs:ident)*|)?
+        $object:ty
+        $(
+           | $field:ident $(. $rest:ident)* => $method_name:ident :: $ty:ty
+        )+
+    ) => {
+        impl $(<$g0 $(, $gs)*>)? $object {
+            $(
+                #[inline]
+                pub fn $method_name(&self) -> &$ty {
+                    &self.$field $(.$rest)*
+                }
+            )+
+        }
+    };
+}
+
+pub fn distribute<X: Copy, Y: Copy>(
+    left: impl ExactSizeIterator + Iterator<Item = X>,
+    mut right: impl ExactSizeIterator + Iterator<Item = Y>,
+) -> Vec<(X, Y)> {
+    // [x1, x2, ..., xn] x [y1, y2, ..., yn] = [(x1, y1), (x1, y2), ..., (x1,
+    // yn), (x2, y1), ..., (x2, yn), ..., (xn, y1), ..., (xn, yn)]
+    let mut pairs = Vec::with_capacity(left.len() * right.len());
+    for x in left {
+        for y in right.by_ref() {
+            pairs.push((x, y))
+        }
+    }
+    pairs
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
