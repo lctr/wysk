c61111e9d99e649e5677d46cefcdc1100dfc710b
Lictor Guzman <lctrgzmn@gmail.com>
2022-06-28 22:49:22 -0500

    added `bump_or_peek_on` and `trailing` methods; added documentation comments

--- compiler/parser/src/stream.rs
+++ compiler/parser/src/stream.rs
@@ -8,6 +8,7 @@ pub trait Streaming {
 
     /// Peeks at the current `Peeked` item and calls the `Lexlike::cmp_tok`
     /// method on the provided input, returning whether a match was found.
+    #[inline]
     fn peek_on<T>(&mut self, item: T) -> bool
     where
         T: Lexlike<Self::Peeked, Self::Lexeme>,
@@ -42,6 +43,7 @@ pub trait Streaming {
     ///     /* do something */
     /// } /* do something else */
     /// ```
+    #[inline]
     fn bump_on<T>(&mut self, item: T) -> bool
     where
         T: Lexlike<Self::Peeked, Self::Lexeme>,
@@ -53,6 +55,28 @@ pub trait Streaming {
         on_it
     }
 
+    /// Shortcut equivalent to calling `bump_on(bump_if) || peek_on(peek_on)`,
+    /// but optimized such that only a single call to the underlying token
+    /// stream is made.
+    #[inline]
+    fn bump_or_peek_on(
+        &mut self,
+        bump_if: impl Lexlike<Self::Peeked, Self::Lexeme>,
+        peek_if: impl Lexlike<Self::Peeked, Self::Lexeme>,
+    ) -> bool {
+        match self.peek() {
+            Some(t) if bump_if.cmp_tok(t) => {
+                self.bump();
+                true
+            }
+            Some(t) => peek_if.cmp_tok(t),
+            _ => false,
+        }
+    }
+
+    /// Conditionally advances the underlying token stream if the current lexeme
+    /// matches the given `item`.
+    #[inline]
     fn ignore<T>(&mut self, item: T)
     where
         T: Lexlike<Self::Peeked, Self::Lexeme>,
@@ -60,6 +84,29 @@ pub trait Streaming {
         self.bump_on(item);
     }
 
+    /// Ignores the lexeme `item` after applying the given closure `f`,
+    /// returning the results. This method is non-strict with regards to the
+    /// lexeme in that it won't fail if the next lexeme in the stream, after
+    /// appplying `f`, doesn't match `item`.
+    ///
+    /// This method can be used to parse a node optionally followed by a
+    /// trailing separator.
+    #[inline]
+    fn trailing<X>(
+        &mut self,
+        end: impl Lexlike<Self::Peeked, Self::Lexeme>,
+        mut f: impl FnMut(&mut Self) -> Result<X, Self::Err>,
+    ) -> Result<X, Self::Err> {
+        let it = f(self)?;
+        self.ignore(end);
+        Ok(it)
+    }
+
+    /// Returns whether the underlying token stream is complete. A token stream
+    /// is considered 'done' if the current token is the `EOF` lexeme. Note that
+    /// there may be subtle differences between calling the token stream's
+    /// `is_done` method and matching the peeked current token with `Eof`, as
+    /// the underlying token stream may have non-EOF tokens queued.
     fn is_done(&mut self) -> bool;
 
     /// Given a predicate `pred` and a parser, applies the given parser `parse`
@@ -71,6 +118,7 @@ pub trait Streaming {
     ///
     /// **Note:** the given predicate *must* be coercible to
     /// `fn` pointer, and hence **must not** capture any variables.
+    #[inline]
     fn many_while<F, G, X>(&mut self, mut pred: G, mut f: F) -> Result<Vec<X>, Self::Err>
     where
         G: FnMut(&mut Self) -> bool,
@@ -83,6 +131,7 @@ pub trait Streaming {
         Ok(xs)
     }
 
+    #[inline]
     fn many_while_on<L, F, X>(&mut self, item: L, mut f: F) -> Result<Vec<X>, Self::Err>
     where
         L: Lexlike<Self::Peeked, Self::Lexeme>,
