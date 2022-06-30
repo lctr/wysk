6ea544bca1dd661c7963eb3d2fdc9429695acaa0
Lictor Guzman <lctrgzmn@gmail.com>
2022-06-29 19:40:02 -0500

    re-exporting all compiler crates as a library

---
+++ compiler/driver/src/lib.rs
@@ -0,0 +1,17 @@
+///! Library entry point providing access to all the modules comprising the
+///! compiler
+pub use wy_check;
+pub use wy_common;
+pub use wy_failure;
+pub use wy_graph;
+pub use wy_intern;
+pub use wy_lexer;
+pub use wy_name;
+pub use wy_parser;
+pub use wy_pretty;
+// pub use wy_resolver;
+pub use wy_session;
+// pub use wy_simple;
+pub use wy_sources;
+pub use wy_span;
+pub use wy_syntax;
