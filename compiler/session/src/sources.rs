8a0fd936897717d4e7f162ec356303a72c77ff9f
Lictor Guzman <lctrgzmn@gmail.com>
2022-07-11 18:58:15 -0500

    docs; wip inspections

--- compiler/session/src/sources.rs
+++ compiler/session/src/sources.rs
@@ -1,24 +1,26 @@
 use wy_check::typed::envr::{self, Environment};
-use wy_failure::{Failure, Outcome};
+use wy_failure::Outcome;
 use wy_parser::error::ParseError;
 use wy_sources::paths::Atlas;
 use wy_syntax::Ast;
 
-pub fn parse_atlas(atlas: &Atlas) -> Result<Ast, Failure<ParseError>> {
-    let mut tree = Ast::new();
-
-    for src in atlas.sources_iter() {
-        let program = wy_parser::try_parse_file(src)?;
-        tree.add_program(program);
-    }
-
-    Ok(tree)
+/// Blindly finds all wysk files contained within a directory, then parsing each
+/// file in the order in which they were found regardless as to whether they are
+/// referenced/are used as dependencies at all.
+pub fn parse_atlas(atlas: &Atlas) -> Outcome<Ast, ParseError> {
+    atlas.sources_iter().fold(Ok(Ast::new()), |tree, src| {
+        tree.and_then(|mut ast| {
+            wy_parser::try_parse_file(src).map(|program| {
+                ast.add_program(program);
+                ast
+            })
+        })
+    })
 }
 
 pub fn parse_prelude() -> Outcome<Ast, ParseError> {
     let atlas = Atlas::new_within_dir("../../language")
         .map_err(wy_failure::normalize_io_err::<ParseError>)?;
-    // let atlas = Atlas::with_prelude()
     parse_atlas(&atlas)
 }
 
@@ -31,6 +33,9 @@ pub fn prelude_environment() -> Outcome<(Ast, Environment), ParseError> {
 
 #[cfg(test)]
 mod test {
+
+    use wy_common::{push_if_absent, Map};
+    use wy_syntax::Program;
 
     use super::*;
 
@@ -52,6 +57,39 @@ mod test {
         match prelude_environment() {
             Ok(env) => println!("{:#?}", env),
             Err(e) => eprintln!("{}", e),
+        }
+    }
+
+    #[test]
+    fn inspect_prelude_deps() {
+        match parse_prelude() {
+            Err(e) => eprintln!("{}", e),
+
+            Ok(tree) => {
+                let imports = tree
+                    .programs_iter()
+                    .map(|prog| {
+                        let ims = prog.get_imports_iter().fold(vec![], push_if_absent);
+                        (prog.module_id(), prog.modname(), ims)
+                    })
+                    .collect::<Vec<_>>();
+                println!("Program Imports");
+                imports.iter().for_each(|(mid, mname, ims)| {
+                    println!("\n  {} = {}", mid, mname);
+                    ims.iter().enumerate().for_each(|(n, imp)| {
+                        println!(
+                            "    | #{}. {} {}",
+                            n,
+                            &imp.name,
+                            if let Some(rename) = &imp.rename {
+                                format!("@ {}", rename)
+                            } else {
+                                String::new()
+                            }
+                        )
+                    });
+                });
+            }
         }
     }
 }
