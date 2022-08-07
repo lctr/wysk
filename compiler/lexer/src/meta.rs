c1557954e01c3b8bdd7c172376f6f0ca6fa12ca5
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-07 01:25:58 -0500

    cleared up and simplified builtin attribute pragmas

--- compiler/lexer/src/meta.rs
+++ compiler/lexer/src/meta.rs
@@ -4,21 +4,11 @@ use wy_span::{Span, Spanned};
 
 use crate::{is_ident_char, Source};
 
-strenum! { Lint is_lint ::
-    All "all"
-    Unused "unused"
-}
-
-impl Symbolic for Lint {
-    fn get_symbol(&self) -> Symbol {
-        Symbol::from_str(self.as_str())
-    }
-}
-
-strenum! { Attr is_attr ::
-    Inline "inline" | "in_line"
+strenum! { Attr is_builtin_attr ::
+    Inline "inline"
     NoInline "noinline" | "no_inline"
     Fixity "fixity" | "infix"
+    Derive "derive"
     Allow "allow"
     Test "test"
     Todo "todo"
@@ -28,144 +18,19 @@ strenum! { Attr is_attr ::
     Macro "macro"
 }
 
-impl Attr {
-    pub fn scan(stream: &mut Source) -> Option<Spanned<Self>> {
-        let mut iter = stream.clone();
-        let (span, _loc) = iter.eat_while(is_ident_char).parts();
-        Self::from_str(&iter[span]).map(|attr| {
-            *stream = iter;
-            Spanned::new(attr, span)
-        })
-    }
-}
-
-impl Symbolic for Attr {
-    fn get_symbol(&self) -> Symbol {
-        Symbol::from_str(self.as_str())
-    }
-}
-
-strenum! { Associativity
-    @test_str: is_assoc
-    Left "L" | "l" | "left" | "LEFT" | "Left" => is_left
-    Right "R" | "r" | "right" | "RIGHT" | "Right" => is_right
-    None "N" | "n" | "none" | "NONE" => is_none
-}
-
-impl Associativity {
-    pub fn scan(stream: &mut Source) -> Option<Spanned<Self>> {
-        let mut iter = stream.clone();
-        let (span, _loc) = iter
-            .eat_while(|c| "lLrRnNeEfFtTiIgGhHtToO".contains(c))
-            .parts();
-        let assoc = Self::from_str(&iter[span]);
-        assoc.map(|assoc| {
-            *stream = iter;
-            Spanned::new(assoc, span)
-        })
-    }
-}
-
-impl Symbolic for Associativity {
-    fn get_symbol(&self) -> Symbol {
-        Symbol::from_str(self.as_str())
-    }
-}
-
-strenum! { Digit is_digit ::
-    Zero  "0"     One "1"
-    Two   "2"     Three "3"
-    Four  "4"     Five "5"
-    Six   "6"     Seven "7"
-    Eight "8"     Nine "9"
-}
-
-impl Digit {
-    pub const DECIMAL: &'static str = "0123456789";
-    #[inline]
-    pub fn digit_char(c: char) -> bool {
-        c.is_digit(10)
-    }
-
-    pub fn scan(stream: &mut Source) -> Option<Spanned<Self>> {
-        let mut iter = stream.clone();
-        iter.eat_whitespace();
-        let start = iter.get_pos();
-        let digit = iter.next().and_then(Self::from_char);
-        let span = iter.span_from(start);
-        digit.map(|digit| {
-            *stream = iter;
-            Spanned::new(digit, span)
-        })
-    }
-}
-
-impl Symbolic for Digit {
-    fn get_symbol(&self) -> Symbol {
-        Symbol::from_str(self.as_str())
-    }
-}
-
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Spec {
+pub enum Pragma {
     BuiltIn(Attr),
     Custom(Symbol),
 }
 
-impl Spec {
-    pub fn scan(stream: &mut Source) -> Option<Spanned<Spec>> {
-        let mut iter = stream.clone();
-        // if let Spanned(Some(spec), span) =
-        let attr = Attr::scan(&mut iter).map(|attr| attr.map(Self::BuiltIn));
-        if attr.is_none() {
-            let mut first = true;
-            let (span, _) = stream
-                .eat_while(|c| {
-                    if first {
-                        first = false;
-                        c.is_alphabetic() || matches!(c, '_')
-                    } else {
-                        c.is_alphanumeric() || matches!(c, '_' | '\'')
-                    }
-                })
-                .parts();
-            if span.is_empty() {
-                None
-            } else {
-                let sym = wy_intern::intern_once(&iter[span]);
-                let spec = Self::Custom(sym);
-                *stream = iter;
-                Some(Spanned(spec, span))
-            }
-        } else {
-            *stream = iter;
-            attr
-        }
-    }
-}
-
-impl Symbolic for Spec {
-    fn get_symbol(&self) -> Symbol {
-        match self {
-            Spec::BuiltIn(attr) => attr.get_symbol(),
-            Spec::Custom(s) => *s,
-        }
-    }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Pragma {
-    Attr(Attr),
-    Allow(Lint),
-    Fixity(Associativity, Digit),
-    Custom(Symbol),
-    Spec(Spec),
-    Error(Span),
-}
-
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Placement {
+    /// Indicates that the relative position of an attribute or compiler
+    /// pragma is *before* the item that it annotates.
     Before,
+    /// Indicates that the relative position of an attribute or
+    /// compiler pragma is *after* the item that it annotates.
     After,
 }
 
