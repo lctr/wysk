fe596552e9322ed9ec5ebc9f70bacc13b10f3e18
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-08 01:51:42 -0500

    project builder

--- compiler/sources/src/lib.rs
+++ compiler/sources/src/lib.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 use files::{File, SourceMap};
 use manifest::Manifest;
-use paths::{Atlas, Dir, FilePath};
+use paths::{Atlas, Directory, FileId, FilePath};
 
 pub mod files;
 pub mod manifest;
@@ -27,29 +27,117 @@ impl Config {
     }
 }
 
+pub struct ProjectBuilder {
+    root_dir: Option<Directory>,
+    atlas: Atlas,
+    manifest: Option<Manifest>,
+    submodules: Vec<FileId>,
+}
+
+impl ProjectBuilder {
+    pub fn new(root_dir: Directory) -> Self {
+        let atlas = Atlas::new();
+        let manifest = None;
+        let submodules = vec![];
+        ProjectBuilder {
+            root_dir: Some(root_dir),
+            atlas,
+            manifest,
+            submodules,
+        }
+    }
+    pub fn read_manifest(mut self) -> Self {
+        match self.root_dir.take() {
+            Some(root_dir) => {
+                self.manifest = root_dir.read_manifest();
+                self.root_dir = Some(root_dir);
+            }
+            None => (),
+        }
+        self
+    }
+
+    pub fn store_manifest_submodules(mut self) -> Self {
+        match self.manifest.take() {
+            Some(manifest) => {
+                manifest
+                    .workspaces()
+                    .flat_map(|p| self.atlas.add_path(p))
+                    .for_each(|f| {
+                        if self.submodules.contains(&f) {
+                            self.submodules.push(f)
+                        }
+                    });
+
+                self.manifest = Some(manifest);
+            }
+            None => (),
+        }
+        self
+    }
+
+    pub fn walk_root_dir(mut self) -> Self {
+        match self.root_dir.take() {
+            Some(dir) => {
+                self.atlas.add_dir(dir.clone());
+                self.root_dir = Some(dir);
+            }
+            None => (),
+        }
+        self
+    }
+
+    pub fn build_project(self) -> Option<Project> {
+        let ProjectBuilder {
+            root_dir,
+            atlas,
+            manifest,
+            submodules: _,
+        } = self;
+        root_dir.map(|root_dir| {
+            let config = manifest
+                .map(Config::Project)
+                .unwrap_or_else(|| Config::Script);
+            let mut source_map = SourceMap::new();
+            let mut submodules = vec![];
+            atlas.sources_iter().for_each(|fp| {
+                if let Ok(file) = source_map.add_from_filepath(fp.clone()) {
+                    submodules.push(file.src_path().file_id())
+                }
+            });
+            Project {
+                atlas,
+                config,
+                source_map,
+                root_dir,
+                submodules,
+            }
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct Project {
     atlas: Atlas,
     config: Config,
     source_map: SourceMap,
+    root_dir: Directory,
+    submodules: Vec<FileId>,
 }
 
 impl Project {
     /// Given a `Dir` instance, finds all Wysk source files contained
     /// (recursively) in either `src` or in the directories listed in the
     /// `workspaces` field of the manifest file `manifest.toml`.
-    pub fn new_from_dir(dir: Dir) -> Option<Project> {
-        let man = dir.read_manifest();
-        let mut atlas = Atlas::walk_dir(dir);
-        man.and_then(|man| {
-            atlas.add_paths(man.workspaces());
-            Some(Config::Project(man))
-        })
-        .map(|config| Project {
-            atlas,
-            config,
-            source_map: SourceMap::new(),
-        })
+    pub fn new_from_dir(dir: Directory) -> Option<Project> {
+        ProjectBuilder::new(dir)
+            .read_manifest()
+            .store_manifest_submodules()
+            .walk_root_dir()
+            .build_project()
+    }
+    pub fn root_dir(&self) -> &Directory {
+        &self.root_dir
     }
 
     pub fn atlas(&self) -> &Atlas {
@@ -79,11 +167,14 @@ impl Project {
     pub fn file_count(&self) -> usize {
         self.source_map.file_count()
     }
+    pub fn submodule_ids(&self) -> &[FileId] {
+        self.submodules.as_slice()
+    }
 }
 
 /// Alias for the associated method `Project::new_from_dir`
 pub fn new_project(path: impl AsRef<Path>) -> Option<Project> {
-    Project::new_from_dir(Dir::new(0, path))
+    Project::new_from_dir(Directory::new(0, path))
 }
 
 #[cfg(test)]
@@ -92,7 +183,7 @@ mod test {
 
     #[test]
     fn inspect_prelude_project() {
-        let dir = Dir::new(0, paths::PRELUDE_PATH);
+        let dir = Directory::new(0, paths::PRELUDE_PATH);
         let project = new_project(dir);
         println!("{:#?}", &project)
     }
