e078e89a198a10f8a85bb37c8901b984546d0020
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-11 19:37:08 -0500

    added builder for styling text

--- compiler/pretty/src/color.rs
+++ compiler/pretty/src/color.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use wy_span::Span;
 
 #[allow(unused)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
@@ -30,55 +29,6 @@ impl Default for AsciiColor {
     }
 }
 
-macro_rules! impl_get_str {
-    (
-        $name:ident fg bg
-        $(| $variant:ident $lit1:literal $lit2:literal)+
-    ) => {
-        impl $name {
-            pub fn fg_val_str(&self) -> &str {
-                match self {
-                    $(Self::$variant => { stringify!($lit1) })+
-                    // should variants provided be exhaustive? or should there
-                    // be a default value?
-                    // #[allow(unreachable_patterns)]
-                    // _ => "0"
-                }
-            }
-            pub fn bg_val_str(&self) -> &str {
-                match self {
-                    $(Self::$variant => { stringify!($lit2)} )+
-                    // if keeping default value, setting it to same as `Default`
-                    // variant
-                    // #[allow(unreachable_patterns)]
-                    // _ => "10"
-                }
-            }
-        }
-    };
-}
-
-impl_get_str! {
-    AsciiColor fg bg
-    | Black 30 40
-    | Red 31 41
-    | Green 32 42
-    | Yellow 33 43
-    | Blue 34 44
-    | Magenta 35 45
-    | Cyan 36 46
-    | White 37 47
-    | LightBlack 90 100
-    | LightRed 91 101
-    | LightGreen 92 102
-    | LightYellow 93 103
-    | LightBlue 94 104
-    | LightMagenta 95 105
-    | LightCyan 96 106
-    | LightWhite 97 107
-    | Default 0 10
-}
-
 impl AsciiColor {
     pub fn as_fg(&self) -> u8 {
         *self as u8
@@ -94,204 +44,244 @@ impl AsciiColor {
 )]
 pub struct RGB(u8, u8, u8);
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[allow(unused)]
-pub enum Style {
-    None,
-    Fg(AsciiColor),
-    Bg(AsciiColor),
-    FgBg { fg: AsciiColor, bg: AsciiColor },
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum AnsiFormat {
+    Reset = 0,
+    Bold = 1,
+    Dim = 2,
+    Italic = 3,
+    Underlined = 4,
+    Blinking = 5,
+    Inverted = 7,
+    Hidden = 8,
+    Strikethrough = 9,
 }
 
-impl std::fmt::Display for Style {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl AnsiFormat {
+    pub fn reset_val(&self) -> &str {
         match self {
-            Style::None => write!(f, "{:?}", self),
-            Style::Fg(c) => write!(f, "Fg({:?})", c),
-            Style::Bg(c) => write!(f, "Bg({:?})", c),
-            Style::FgBg { fg, bg } => write!(f, "FgBg({:?}, {:?})", fg, bg),
+            AnsiFormat::Reset => "",
+            AnsiFormat::Bold => "22",
+            AnsiFormat::Dim => "22",
+            AnsiFormat::Italic => "23",
+            AnsiFormat::Underlined => "24",
+            AnsiFormat::Blinking => "25",
+            AnsiFormat::Inverted => "27",
+            AnsiFormat::Hidden => "28",
+            AnsiFormat::Strikethrough => "29",
         }
     }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Style {
+    reset_prev: bool,
+    is_bold: bool,
+    is_dim: bool,
+    is_italic: bool,
+    is_underlined: bool,
+    is_blinking: bool,
+    is_inverted: bool,
+    is_hidden: bool,
+    is_strikethrough: bool,
+    fg_color: Option<AsciiColor>,
+    bg_color: Option<AsciiColor>,
 }
 
 impl Default for Style {
     fn default() -> Self {
-        Self::None
+        Self {
+            reset_prev: true,
+            is_bold: false,
+            is_dim: false,
+            is_italic: false,
+            is_underlined: false,
+            is_blinking: false,
+            is_inverted: false,
+            is_hidden: false,
+            is_strikethrough: false,
+            fg_color: None,
+            bg_color: None,
+        }
     }
 }
 
 impl Style {
-    pub fn is_none(&self) -> bool {
-        matches!(self, Self::None)
-    }
-    /// Modifies a provided string buffer in place, applying the given style to
-    /// the `snip` string slice provided.
-    pub fn write_str(&self, buf: &mut String, snip: &str) {
-        match self {
-            Style::None => {
-                buf.push_str(snip);
-            }
-            Style::Fg(color) => {
-                buf.push_str(COLOR_ESC[0]);
-                buf.push_str(color.fg_val_str());
-                buf.push_str(COLOR_ESC[1]);
-                buf.push_str(snip);
-                buf.push_str(COLOR_ESC[2]);
-            }
-            Style::Bg(color) => {
-                buf.push_str(COLOR_ESC[0]);
-                buf.push_str(color.bg_val_str());
-                buf.push_str(COLOR_ESC[1]);
-                buf.push_str(snip);
-                buf.push_str(COLOR_ESC[2]);
-            }
-            Style::FgBg { fg, bg } => {
-                buf.push_str(COLOR_ESC[0]);
-                buf.push_str(bg.bg_val_str());
-                buf.push(';');
-                buf.push_str(fg.fg_val_str());
-                buf.push_str(COLOR_ESC[1]);
-                buf.push_str(snip);
-                buf.push_str(COLOR_ESC[2]);
-            }
+    pub fn codes(&self) -> Vec<u8> {
+        let mut parts = vec![];
+        if self.is_bold {
+            parts.push(AnsiFormat::Bold as u8)
         }
+        if self.is_dim {
+            parts.push(AnsiFormat::Dim as u8)
+        }
+        if self.is_italic {
+            parts.push(AnsiFormat::Italic as u8)
+        }
+        if self.is_underlined {
+            parts.push(AnsiFormat::Underlined as u8)
+        }
+        if self.is_blinking {
+            parts.push(AnsiFormat::Blinking as u8)
+        }
+        if self.is_inverted {
+            parts.push(AnsiFormat::Inverted as u8)
+        }
+        if self.is_hidden {
+            parts.push(AnsiFormat::Hidden as u8)
+        }
+        if self.is_strikethrough {
+            parts.push(AnsiFormat::Strikethrough as u8)
+        }
+        if let Some(fg) = self.fg_color {
+            parts.push(fg.as_fg())
+        }
+        if let Some(bg) = self.bg_color {
+            parts.push(bg.as_bg())
+        }
+        parts
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum TextError {
-    StyleBound {
-        style: Style,
-        bounds: Span,
-        strlen: usize,
-    },
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct StyleBuilder(Style);
+
+impl Default for StyleBuilder {
+    fn default() -> Self {
+        Self(Default::default())
+    }
 }
 
-impl std::fmt::Display for TextError {
+impl StyleBuilder {
+    pub fn new() -> Self {
+        Self(Style::default())
+    }
+    pub fn set_bold(mut self, is_bold: bool) -> Self {
+        self.0.is_bold = is_bold;
+        self
+    }
+    pub fn set_dim(mut self, is_dim: bool) -> Self {
+        self.0.is_dim = is_dim;
+        self
+    }
+    pub fn set_italic(mut self, is_italic: bool) -> Self {
+        self.0.is_italic = is_italic;
+        self
+    }
+    pub fn set_blinking(mut self, is_blinking: bool) -> Self {
+        self.0.is_blinking = is_blinking;
+        self
+    }
+    pub fn set_inverted(mut self, is_inverted: bool) -> Self {
+        self.0.is_inverted = is_inverted;
+        self
+    }
+    pub fn set_hidden(mut self, is_hidden: bool) -> Self {
+        self.0.is_hidden = is_hidden;
+        self
+    }
+    pub fn set_strikethrough(mut self, is_strikethrough: bool) -> Self {
+        self.0.is_strikethrough = is_strikethrough;
+        self
+    }
+    pub fn set_fg_color(mut self, color: AsciiColor) -> Self {
+        self.0.fg_color = Some(color);
+        self
+    }
+    pub fn set_bg_color(mut self, color: AsciiColor) -> Self {
+        self.0.bg_color = Some(color);
+        self
+    }
+    pub fn build<S>(self, s: S) -> Styled<S> {
+        Styled(self.0, s)
+    }
+}
+
+pub struct Styled<S>(Style, S);
+
+impl<S> Styled<Vec<S>> {
+    pub fn apply_to_all(self) -> Vec<Styled<S>> {
+        let style = self.0;
+        self.1.into_iter().map(|s| Styled(style, s)).collect()
+    }
+}
+
+impl<S> Styled<S> {
+    pub fn modify_style(self, f: impl FnOnce(StyleBuilder) -> StyleBuilder) -> Self {
+        let style = self.0;
+        let mut styler = StyleBuilder::new();
+        styler.0 = style;
+        f(styler).build(self.1)
+    }
+
+    pub fn fmt_display(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    where
+        S: std::fmt::Display,
+    {
+        std::fmt::Display::fmt(self, f)
+    }
+
+    pub fn fmt_debug(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    where
+        S: std::fmt::Debug,
+    {
+        std::fmt::Debug::fmt(self, f)
+    }
+}
+
+impl<S> std::fmt::Debug for Styled<S>
+where
+    S: std::fmt::Debug,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TextError::StyleBound { style, bounds: Span(start, end), strlen } => write!(f, "TextError: Style is out of bounds! The buffer string has a length of {} bytes, but the style `{}` has spans over the byte range {} .. {}.", strlen, style, start, end),
+        let parts = self.0.codes();
+        let item = &self.1;
+        match &parts[..] {
+            [] => write!(f, "\u{1b}[0m{item:?}"),
+            [a, bs @ ..] => {
+                write!(f, "\u{1b}[{a}")?;
+                for b in bs {
+                    write!(f, ";{b}")?;
+                }
+                write!(f, "m{item:?}\u{1b}[0m")
+            }
         }
     }
 }
 
-impl std::error::Error for TextError {}
-
-/// Bases used in constructing colored text.
-pub const COLOR_ESC: [&str; 3] = ["\u{1b}[0m\u{1b}[", ";15m", "\u{1b}[0m"];
-
-type IoTxt = &'static str;
-
-pub const RED_PARENS: [IoTxt; 2] = [
-    concat!("\u{1b}[0m\u{1b}[", 31, ";15m", "(", "\u{1b}[0m"),
-    concat!("\u{1b}[0m\u{1b}[", 31, ";15m", ")", "\u{1b}[0m"),
-];
-
-pub const RED_WHITE_NAME: IoTxt = concat!(
-    "\u{1b}[0m\u{1b}[",
-    31,
-    ";15m",
-    "(",
-    "\u{1b}[0m",
-    "Wysk",
-    "\u{1b}[0m\u{1b}[",
-    31,
-    ";15m",
-    ")",
-    "\u{1b}[0m"
-);
-
-pub const BLUE_PROMPT_INPUT: IoTxt =
-    concat!("\u{1b}[0m\u{1b}[", 34, ";15m", "<)", "\u{1b}[0m", ' ');
-
-pub const RED_PROMPT_ERROR: IoTxt =
-    concat!("\u{1b}[0m\u{1b}[", 91, ";15m", "Error!", "\u{1b}[0m", ' ');
-
-pub const GREEN_PROMPT_OUT: IoTxt = concat!("\u{1b}[0m\u{1b}[", 32, ";15m", "(>", "\u{1b}[0m", ' ');
-
-/// Strips a string of its ASCII color formatting. This is necessary when
-/// looking to use dispay a string that may have been styled via `Debug`    .
-pub fn strip_color(s: String) -> String {
-    let mut stripped = String::new();
-    let mut tmp = 0;
-    while let Some(a) = (&s[tmp..]).find(COLOR_ESC[0]) {
-        stripped.push_str(&s[tmp..a]);
-        let a_after = a + COLOR_ESC[0].len();
-        match (&s[a_after..]).find(COLOR_ESC[1]) {
-            Some(b) => {
-                let b_after = b + COLOR_ESC[1].len();
-                match (&s[b_after..]).find(COLOR_ESC[2]) {
-                    Some(c) => {
-                        let c_after = c + COLOR_ESC[2].len();
-                        stripped.push_str(&s[c..c_after]);
-                        tmp = c_after;
-                    }
-                    None => {
-                        stripped.push_str(&s[b_after..]);
-                        break;
-                    }
+impl<S> std::fmt::Display for Styled<S>
+where
+    S: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let parts = self.0.codes();
+        let item = &self.1;
+        match &parts[..] {
+            [] => write!(f, "\u{1b}[0m{item}"),
+            [a, bs @ ..] => {
+                write!(f, "\u{1b}[{a}")?;
+                for b in bs {
+                    write!(f, ";{b}")?;
                 }
-            }
-            None => {
-                stripped.push_str(&s[a_after..]);
-                break;
+                write!(f, "m{item}\u{1b}[0m")
             }
         }
     }
-    stripped
 }
 
 #[macro_export]
 macro_rules! color {
     (fg $color:ident $string:expr) => {
-        format!(
-            "\u{1b}[0m\u{1b}[{};15m{}\u{1b}[0m",
-            ($crate::color::AsciiColor::$color as u8),
-            $string
-        )
+        $crate::color::StyleBuilder::new()
+            .set_fg_color($crate::color::AsciiColor::$color)
+            .build($string)
     };
     (bg $color:ident $string:expr) => {
-        format!(
-            "\u{1b}[0m\u{1b}[{};15m{}\u{1b}[0m",
-            ($crate::color::AsciiColor::$color as u8) + 10,
-            $string
-        )
+        $crate::color::StyleBuilder::new()
+            .set_bg_color($crate::color::AsciiColor::$color)
+            .build($string)
     };
-    (wrap $color:ident [$start:expr, $end:expr] $body:expr) => {{
-        use $crate::color::AsciiColor::*;
-        [color!(fg $color $start), $body,
-        color!(fg $color $end)].concat()
-    }};
-    (each fg $($fg:ident $string:expr)+) => {{
-        use $crate::color::AsciiColor::*;
-        let mut s = vec![];
-
-        s.push($(format!(
-            "\u{1b}[0m\u{1b}[{};{};15m{}\u{1b}[0m",
-            ($bg as u8) + 10,
-            $fg as u8,
-            $string
-        ))+);
-        s
-    }};
-    (each $((fg $fg:ident, bg $bg:ident) $string:expr)+) => {{
-        use $crate::color::AsciiColor::*;
-        let mut s = vec![];
-
-        s.push($(format!(
-            "\u{1b}[0m\u{1b}[{};{};15m{}\u{1b}[0m",
-            ($bg as u8) + 10,
-            $fg as u8,
-            $string
-        ))+);
-        s
-    }};
-    (fg for $s:ident match $base:ident $($($ps:pat)+ => $fg:ident),+) => {{
-        match $base {
-            $($($ps)+ => {color!(fg $fg $s)}),+
-            _ => color!(fg Yellow format!("{}", $s))
-        }
-    }};
 }
 
 #[cfg(test)]
@@ -299,13 +289,11 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_strip_color() {
-        let base = "Hello!";
-        let green = color!(fg Green base);
-        println!("(green) display: {}", &green);
-        println!("(green) debug: {:?}", &green);
-        let stripped = strip_color(green);
-        println!("(stripped) display: {}", &stripped);
-        println!("(stripped) debug: {:?}", &stripped);
+    fn expect_bold_red_text() {
+        let styled = StyleBuilder::new()
+            .set_bold(true)
+            .set_fg_color(AsciiColor::Red)
+            .build("hello");
+        println!("{styled}")
     }
 }
