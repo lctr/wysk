903d6824fe675ba98859f1934960ea31227a0616
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-11 19:50:52 -0500

    separated general failure type and error dialogue display

--- compiler/failure/src/lib.rs
+++ compiler/failure/src/lib.rs
@@ -1,3 +1,7 @@
+pub mod show;
+
+pub use show::{Dialogue, SrcLoc, SrcPath};
+
 pub type Outcome<X, E> = Result<X, Failure<E>>;
 
 #[derive(Debug)]
@@ -9,73 +13,6 @@ pub enum Failure<E> {
 pub fn normalize_io_err<E>(error: Failure<std::io::Error>) -> Failure<E> {
     match error {
         Failure::Io(e) | Failure::Err(e) => Failure::Io(e),
-    }
-}
-
-pub struct It<X>(pub(crate) X);
-impl<X> From<X> for It<X> {
-    fn from(x: X) -> Self {
-        Self(x)
-    }
-}
-
-impl<X> AsRef<X> for It<X> {
-    fn as_ref(&self) -> &X {
-        &self.0
-    }
-}
-
-impl<X> std::ops::Deref for It<X> {
-    type Target = X;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<X> std::ops::DerefMut for It<X> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<X: Copy> Copy for It<X> {}
-
-impl<X: Clone> Clone for It<X> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone())
-    }
-}
-
-impl<X: PartialEq> PartialEq for It<X> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl<X: Eq> Eq for It<X> {}
-
-impl<X: std::hash::Hash> std::hash::Hash for It<X> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.0.hash(state);
-    }
-}
-
-impl<X: Default> Default for It<X> {
-    fn default() -> Self {
-        Self(Default::default())
-    }
-}
-
-impl<X: std::fmt::Debug> std::fmt::Debug for It<X> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        X::fmt(&self.0, f)
-    }
-}
-
-impl<X: std::fmt::Display> std::fmt::Display for It<X> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        X::fmt(&self.0, f)
     }
 }
 
@@ -107,17 +44,6 @@ impl<E> From<Failure<Failure<E>>> for Failure<E> {
 }
 
 impl<E> std::error::Error for Failure<E> where E: std::error::Error {}
-
-#[macro_export]
-macro_rules! fails {
-    (impl for $ty:ty $(| $val:ty)?) => {
-        impl From<$ty> for $crate::Failure<$ty $(, $val)?> {
-            fn from(it: $ty) -> Self {
-                $crate::Failure::Err(it)
-            }
-        }
-    };
-}
 
 #[cfg(test)]
 mod tests {
---
+++ compiler/failure/src/show.rs
@@ -0,0 +1,235 @@
+use std::path::{Path, PathBuf};
+
+use wy_pretty::color::{AsciiColor, StyleBuilder};
+use wy_span::{Col, Coord, Row, Span};
+
+const PAD_SPACE: usize = 4;
+
+// Glyphs used in printing
+const PIPE: char = '│';
+const FORK: char = '├';
+const DASH: char = '─';
+const CORNER: char = '┌';
+const CARET: char = '^';
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Glyphs(&'static [char]);
+
+impl std::fmt::Display for Glyphs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for c in self.0 {
+            write!(f, "{c}")?;
+        }
+        Ok(())
+    }
+}
+
+const TOP_LEFT: Glyphs = Glyphs(&[CORNER, DASH]);
+
+struct Repeat<S>(usize, S);
+
+impl<S> std::fmt::Display for Repeat<S>
+where
+    S: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut count = self.0;
+        let it = &self.1;
+        while count > 0 {
+            write!(f, "{it}")?;
+            count -= 1;
+        }
+        Ok(())
+    }
+}
+
+impl<S> std::fmt::Debug for Repeat<S>
+where
+    S: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut count = self.0;
+        let it = &self.1;
+        while count > 0 {
+            write!(f, "{it}")?;
+            count -= 1;
+        }
+        Ok(())
+    }
+}
+
+/// Describing the source path and position, primarily used in error reporting.
+/// This should be included in every error message to be able to reproduce
+/// tracking information regarding the source code involved during error
+/// reporting.
+///
+/// The error should effectively be able to produce a string of the form
+/// ```txt
+///         [PATH/TO/FILE]:[ROW][COL]
+/// ```
+/// in error messages.
+pub struct Dialogue<'s, S: AsRef<str> = String> {
+    label: &'s str,
+    message: String,
+    srctext: &'s S,
+    srcloc: &'s SrcLoc,
+    span: Span,
+}
+
+impl<'s, S: AsRef<str>> Dialogue<'s, S> {
+    pub fn new(
+        label: &'s str,
+        message: String,
+        srctext: &'s S,
+        srcloc: &'s SrcLoc,
+        span: Span,
+    ) -> Self {
+        Dialogue {
+            label,
+            message,
+            srctext,
+            srcloc,
+            span,
+        }
+    }
+
+    pub fn display(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(self, f)
+    }
+
+    pub fn row_col(&self) -> (Row, Col) {
+        let Coord { row, col } = self.srcloc.coord;
+        (row, col)
+    }
+
+    pub fn gutter(&self) -> RowGutter {
+        self.srcloc.gutter()
+    }
+}
+
+impl Dialogue<'_> {
+    pub fn write_bold_red(
+        msg: impl std::fmt::Display,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        StyleBuilder::new()
+            .set_fg_color(AsciiColor::Red)
+            .set_bold(true)
+            .build(msg)
+            .fmt_display(f)
+    }
+}
+
+impl<'s, S: AsRef<str>> std::fmt::Display for Dialogue<'s, S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // error label
+        let label = StyleBuilder::new()
+            .set_bold(true)
+            .set_fg_color(AsciiColor::Red)
+            .build(self.label);
+        let message = StyleBuilder::new().set_bold(true).build(&self.message);
+
+        let srcloc = self.srcloc;
+        let (row, col) = self.row_col();
+        let gutter = srcloc.gutter();
+        // TODO: sometimes the error will be on the beginning of the next line
+        // (or end of previous line??) with no line text shown at all.
+        // womp. fix.
+        let line = &&self.srctext.as_ref().lines()[row - 1usize];
+        let trimmed = line.trim();
+        // string with whitespace trimmed is *never* longer than the original
+        let diff = line.len() - trimmed.len();
+        let caretlen = self.span.len();
+        let caret_offset = if caretlen == 1 { 1 } else { (diff + 2) / 2 };
+        let dash_len = PAD_SPACE + caret_offset + col.as_usize().abs_diff(diff.abs_diff(caretlen));
+        let midline = Repeat(dash_len, DASH);
+        let carets = StyleBuilder::new()
+            .set_fg_color(AsciiColor::Red)
+            .build(Repeat(caretlen, CARET));
+
+        write!(
+            f,
+            "{label} {message}\n\
+             {gutter}{TOP_LEFT}{srcloc}\n\
+             {gutter}{PIPE}\n \
+            [{row}]  {PIPE}    `{trimmed}`\n\
+             {gutter}{FORK}{midline}{carets}\n\
+             {gutter}{PIPE}\n"
+        )
+    }
+}
+
+impl<'s, S: AsRef<str>> std::fmt::Debug for Dialogue<'s, S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", &self)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum SrcPath<P: AsRef<Path> = PathBuf> {
+    Direct,
+    File(P),
+}
+
+impl<P: AsRef<Path>> Default for SrcPath<P> {
+    fn default() -> Self {
+        Self::Direct
+    }
+}
+
+impl<P: AsRef<Path>> std::fmt::Display for SrcPath<P> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SrcPath::Direct => write!(f, "<{}>", wy_pretty::color!(fg Red "STDIN")),
+            SrcPath::File(p) => write!(f, "{}", p.as_ref().display()),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub struct SrcLoc {
+    pub coord: Coord,
+    pub pathstr: SrcPath<PathBuf>,
+}
+
+impl std::fmt::Display for SrcLoc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // include only starting coordinates?
+        write!(f, "{}:{}", &self.pathstr, &self.coord)
+    }
+}
+
+impl std::fmt::Debug for SrcLoc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl SrcLoc {
+    pub fn gutter(&self) -> RowGutter {
+        RowGutter(self.coord.row)
+    }
+}
+
+/// Error printing utility
+#[derive(Copy, Clone, PartialEq, PartialOrd)]
+pub struct RowGutter(Row);
+impl std::fmt::Display for RowGutter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for _ in 0..(PAD_SPACE + self.0.strlen() as usize) {
+            char::fmt(&' ', f)?;
+        }
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! fails {
+    (impl for $ty:ty $(| $val:ty)?) => {
+        impl From<$ty> for $crate::Failure<$ty $(, $val)?> {
+            fn from(it: $ty) -> Self {
+                $crate::Failure::Err(it)
+            }
+        }
+    };
+}
