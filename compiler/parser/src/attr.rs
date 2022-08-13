d7a321cfdbc6686dc8d9f549965a907d401b2771
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-13 14:47:05 -0500

    added support for attributes

--- compiler/parser/src/attr.rs
+++ compiler/parser/src/attr.rs
@@ -1,29 +1,271 @@
 #![allow(unused)]
 use wy_lexer::{
-    meta::{Attr, Placement, Pragma},
+    comment::LineKind,
+    meta::{Attr, Meta, Placement},
     stream::Mode,
-    Lexeme,
+    token::Not,
+    Keyword, Lexeme, Token,
 };
 use wy_name::Ident;
-use wy_syntax::attr::Attribute;
+use wy_span::{BytePos, Span, Spanned, WithSpan};
+use wy_syntax::{
+    attr::{Attribute, DocLine, Pragma},
+    fixity::{Assoc, Fixity, Prec},
+    SpannedIdent,
+};
 
-// use crate::error::Parsed;
+use crate::error::*;
 
 use super::stream::*;
 
 impl<'t> Parser<'t> {
-    pub fn attribute_node(&mut self) {
-        match self.lexer.get_mode() {
-            Mode::Default => todo!(),
-            Mode::Meta {
-                place: _,
-                attr_seen: _,
-            } => {
-                todo!()
-                // self.pragma(*place)
-            }
-            Mode::Macro => todo!(),
+    pub fn module_doc_comments(&mut self) -> Vec<Pragma<SpannedIdent, SpannedIdent>> {
+        let (reg_comments, doc_comments) = std::mem::take(&mut self.lexer.comments)
+            .into_iter()
+            .fold((vec![], vec![]), |(mut reg, mut doc), com| {
+                match com {
+                    c @ (wy_syntax::Comment::Line(_)
+                    | wy_syntax::Comment::Block(_)
+                    | wy_syntax::Comment::Doc {
+                        linekind: LineKind::Ignore,
+                        ..
+                    }) => reg.push(c),
+                    wy_syntax::Comment::Doc { id, span, linekind } => doc.push(Pragma {
+                        span,
+                        plmt: Placement::Before,
+                        attr: Attribute::Doc(DocLine {
+                            id,
+                            span,
+                            line_kind: linekind,
+                        }),
+                    }),
+                };
+                (reg, doc)
+            });
+        self.lexer.comments = reg_comments;
+        doc_comments
+    }
+
+    pub fn attributes(&mut self) -> Parsed<Vec<Pragma<SpannedIdent, SpannedIdent>>> {
+        let (is_before, is_after) = {
+            let mode = self.lexer.get_mode();
+            (mode.is_meta_before(), mode.is_meta_after())
+        };
+        match self.peek() {
+            Some(Token {
+                lexeme: Lexeme::Pound,
+                ..
+            }) if is_before => self.attr_before(),
+            Some(Token {
+                lexeme: Lexeme::Hashbang,
+                ..
+            }) if is_after => self.attr_after(),
+            _ => Ok(vec![]),
         }
-        // self.eat(Lexeme::BrackL)?;
+    }
+
+    /// Continuously parses attribute pragmas that precede the
+    /// declaration they describe
+    pub fn attr_before(&mut self) -> Parsed<Vec<Pragma<SpannedIdent, SpannedIdent>>> {
+        let mut prags = vec![];
+        while self.peek_on(Lexeme::Pound) {
+            let start = self.get_pos();
+            self.eat(Lexeme::Pound)?;
+            self.eat(Lexeme::BrackL)?;
+            let prag = self.pragma(start, Placement::Before)?;
+            prags.push(prag);
+        }
+        Ok(prags)
+    }
+
+    /// Parses attribute pragmas that follow the declaration they describe
+    pub fn attr_after(&mut self) -> Parsed<Vec<Pragma<SpannedIdent, SpannedIdent>>> {
+        let mut prags = vec![];
+        while self.peek_on(Lexeme::Hashbang) && self.lexer.get_mode().is_meta_after() {
+            let start = self.get_pos();
+            self.eat(Lexeme::Hashbang)?;
+            self.eat(Lexeme::BrackL)?;
+            let prag = self.pragma(start, Placement::After)?;
+            prags.push(prag);
+        }
+        Ok(prags)
+    }
+
+    pub fn pragma(
+        &mut self,
+        start: BytePos,
+        plmt: Placement,
+    ) -> Parsed<Pragma<SpannedIdent, SpannedIdent>> {
+        let attr = match self.peek() {
+            Some(Token {
+                lexeme: Lexeme::Meta(meta),
+                ..
+            }) => {
+                let meta = *meta;
+                self.meta_head(meta)
+            }
+            Some(Token {
+                lexeme: Lexeme::Eof,
+                ..
+            })
+            | None => self.unexpected_eof().err(),
+            Some(t) if t.is_brack_r() => self
+                .custom_error("while parsing attribute; cannot have empty pragma or attribute")
+                .err(),
+            _ => self
+                .custom_error("is not a valid pragma or attribute name")
+                .err(),
+        }?;
+        self.eat(Lexeme::BrackR)?;
+        self.lexer.reset_mode();
+        let span = Span(start, self.get_pos());
+        Ok(Pragma { span, plmt, attr })
+    }
+
+    pub fn meta_head(&mut self, meta: Meta) -> Parsed<Attribute<SpannedIdent, SpannedIdent>> {
+        match meta {
+            Meta::BuiltIn(attr) => match attr {
+                Attr::Inline => Ok(Attribute::Inline),
+                Attr::NoInline => Ok(Attribute::NoInline),
+                Attr::Fixity => {
+                    self.bump();
+                    self.fixity_attr()
+                }
+                Attr::Derive => {
+                    self.bump();
+                    self.derive_attr()
+                }
+                Attr::Allow => todo!("lint attributes not yet implemented"),
+                Attr::Test => Ok(Attribute::Test),
+                Attr::Todo => Ok(Attribute::Todo),
+                Attr::Specialize => todo!(),
+                Attr::Feature => todo!("feature attributes not yet implemented"),
+                Attr::Cfg => todo!("config attributes not yet implemented"),
+                Attr::Macro => todo!("macro attributes not yet implemented"),
+            },
+            Meta::Custom(s) => {
+                let s = Ident::Label(s);
+                let start = self.get_pos();
+                self.bump();
+                let span = Span(start, self.get_pos());
+                let rest = self.many_while_on(Not(Lexeme::BrackR), |p| Ok(p.bump()))?;
+                Ok(Attribute::Custom(Spanned(s, span), rest))
+            }
+        }
+    }
+
+    /// Updates the parser's internal fixity table upon seeing a
+    /// fixity attribute; Note that fixity attributes are only allowed
+    /// for top-level function declarations!
+    pub fn fixity_attr(&mut self) -> Parsed<Attribute<SpannedIdent, SpannedIdent>> {
+        fn valid_assoc(parser: &mut Parser, first: bool) -> Parsed<Assoc> {
+            match parser.peek() {
+                Some(Token {
+                    lexeme: Lexeme::Kw(Keyword::Infix),
+                    ..
+                }) => Ok(Assoc::None),
+                Some(Token {
+                    lexeme: Lexeme::Kw(Keyword::InfixL),
+                    ..
+                }) => Ok(Assoc::Left),
+                Some(Token {
+                    lexeme: Lexeme::Kw(Keyword::InfixR),
+                    ..
+                }) => Ok(Assoc::Right),
+                Some(Token {
+                    lexeme: Lexeme::Upper(s) | Lexeme::Lower(s),
+                    ..
+                }) => match s.as_str() {
+                    "l" | "L" | "left" | "Left" | "LEFT" => Ok(Assoc::Left),
+                    "r" | "R" | "right" | "Right" | "RIGHT" => Ok(Assoc::Right),
+                    "n" | "N" | "none" | "None" | "NONE" => Ok(Assoc::None),
+                    _ => {
+                        return parser
+                            .custom_error("invalid associativity value for fixity attribute")
+                            .err()
+                    }
+                },
+                Some(t) if t.is_brack_r() => {
+                    if first {
+                        parser.custom_error("prematurely closing fixity attribute while expecting associativity").err()
+                    } else {
+                        Ok(Assoc::None)
+                    }
+                }
+                None
+                | Some(Token {
+                    lexeme: Lexeme::Eof,
+                    ..
+                }) => parser.unexpected_eof().err(),
+                Some(t) => parser
+                    .custom_error("invalid associativity value for fixity attribute")
+                    .err(),
+            }
+        }
+
+        match self.peek() {
+            None |
+            Some(Token { lexeme: Lexeme::Eof, .. }) => {
+                self.unexpected_eof().err()
+            }
+            Some(t) if t.is_brack_r() => {
+                self.custom_error("prematurely closing fixity attribute").err()
+            }
+            Some(Token { lexeme: Lexeme::Lit(lit), ..}) if lit.is_bare_int() => {
+                let prec = self.fixity_prec()?;
+                let assoc = valid_assoc(self, false)?;
+                self.bump();
+                let fixity = Fixity { assoc, prec };
+                Ok(Attribute::Fixity(fixity))
+            }
+            Some(Token { lexeme: Lexeme::Upper(_) | Lexeme::Lower(_), ..}) => {
+                let assoc = valid_assoc(self, true)?;
+                self.bump();
+                let prec = match self.peek() {
+                    Some(Token { lexeme: Lexeme::Lit(lit), .. }) if lit.is_bare_int() => {
+                        self.fixity_prec()
+                    }
+                    Some(t) if t.is_brack_r() => {
+                        Ok(Prec::MAX)
+                    }
+                    None | Some(Token { lexeme: Lexeme::Eof, .. }) => {
+                        self.unexpected_eof().err()
+                    }
+                    _ => {
+                        self.custom_error("is not a valid precedence value; expected a digit between 0 and 9 or a closing right bracket").err()
+                    }
+                }?;
+                let fixity = Fixity { assoc, prec };
+                Ok(Attribute::Fixity(fixity))
+            }
+            // report invalid fixity pragma value
+            _ => self.custom_error("is not a valid fixity attribute parameter, expected -- in any order -- a single digit and/or one of: `l`, `L`, `left`, `Left`, `LEFT`, `infixl`, `r`, `R`, `right`, `Right`, `RIGHT`, `infixr`, `n`, `N`, `none`, `None`, `NONE` or `infix`").err()
+        }
+    }
+
+    pub fn derive_attr(&mut self) -> Parsed<Attribute<SpannedIdent, SpannedIdent>> {
+        self.delimited(
+            [Lexeme::ParenL, Lexeme::Comma, Lexeme::ParenR],
+            Self::expect_upper,
+        )
+        .map(Attribute::Derive)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_fixity_attr() {
+        let src = "\
+        #[fixity 3 L]";
+        let mut parser = Parser::from_str(src);
+        println!("lexer mode: {:?}", parser.lexer.get_mode());
+        parser.peek();
+        println!("lexer mode: {:?}", parser.lexer.get_mode());
+        let res = parser.attributes().unwrap();
+        println!("{:?}", res);
+        println!("{:?}", &parser.lexer)
     }
 }
--- compiler/syntax/src/attr.rs
+++ compiler/syntax/src/attr.rs
@@ -1,20 +1,72 @@
+use serde::{Deserialize, Serialize};
 use wy_common::functor::{MapFst, MapSnd};
+// use wy_intern::Symbol;
 
-use crate::tipo::Type;
+pub use wy_lexer::{
+    comment::{CommentId, LineKind},
+    meta::Placement,
+    Token,
+};
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Attribute<Id, V> {
-    Inline(Id),
-    NoInline(Id),
-    Specialize(Id, Vec<Type<Id, V>>),
+use wy_span::Span;
+
+use crate::{fixity::Fixity, tipo::Type};
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Pragma<Id, V> {
+    pub span: Span,
+    pub plmt: Placement,
+    pub attr: Attribute<Id, V>,
 }
 
-impl<Id, V> Attribute<Id, V> {
-    pub fn name(&self) -> &Id {
-        match self {
-            Attribute::Inline(id) | Attribute::NoInline(id) | Attribute::Specialize(id, _) => id,
+impl<Id, V, X> MapFst<Id, X> for Pragma<Id, V> {
+    type WrapFst = Pragma<X, V>;
+
+    fn map_fst<F>(self, f: &mut wy_common::functor::Func<'_, F>) -> Self::WrapFst
+    where
+        F: FnMut(Id) -> X,
+    {
+        Pragma {
+            span: self.span,
+            plmt: self.plmt,
+            attr: self.attr.map_fst(f),
         }
     }
+}
+
+impl<Id, V, X> MapSnd<V, X> for Pragma<Id, V> {
+    type WrapSnd = Pragma<Id, X>;
+
+    fn map_snd<F>(self, f: &mut wy_common::functor::Func<'_, F>) -> Self::WrapSnd
+    where
+        F: FnMut(V) -> X,
+    {
+        Pragma {
+            span: self.span,
+            plmt: self.plmt,
+            attr: self.attr.map_snd(f),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct DocLine {
+    pub id: CommentId,
+    pub span: Span,
+    pub line_kind: LineKind,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Attribute<Id, V> {
+    Test,
+    Todo,
+    Inline,
+    NoInline,
+    Doc(DocLine),
+    Fixity(Fixity),
+    Derive(Vec<Id>),
+    Specialize(Id, Vec<Type<Id, V>>),
+    Custom(Id, Vec<Token>),
 }
 
 impl<Id, V, X> MapFst<Id, X> for Attribute<Id, V> {
@@ -25,9 +77,17 @@ impl<Id, V, X> MapFst<Id, X> for Attribute<Id, V> {
         F: FnMut(Id) -> X,
     {
         match self {
-            Attribute::Inline(id) => Attribute::Inline(f.apply(id)),
-            Attribute::NoInline(id) => Attribute::NoInline(f.apply(id)),
+            Attribute::Test => Attribute::Test,
+            Attribute::Todo => Attribute::Todo,
+            Attribute::Inline => Attribute::Inline,
+            Attribute::NoInline => Attribute::NoInline,
+            Attribute::Doc(doc) => Attribute::Doc(doc),
+            Attribute::Fixity(fixity) => Attribute::Fixity(fixity),
+            Attribute::Derive(ids) => {
+                Attribute::Derive(ids.into_iter().map(|id| f.apply(id)).collect())
+            }
             Attribute::Specialize(id, tys) => Attribute::Specialize(f.apply(id), tys.map_fst(f)),
+            Attribute::Custom(id, tokens) => Attribute::Custom(f.apply(id), tokens),
         }
     }
 }
@@ -40,9 +100,15 @@ impl<Id, V, X> MapSnd<V, X> for Attribute<Id, V> {
         F: FnMut(V) -> X,
     {
         match self {
-            Attribute::Inline(id) => Attribute::Inline(id),
-            Attribute::NoInline(id) => Attribute::NoInline(id),
+            Attribute::Test => Attribute::Test,
+            Attribute::Todo => Attribute::Todo,
+            Attribute::Inline => Attribute::Inline,
+            Attribute::NoInline => Attribute::NoInline,
+            Attribute::Doc(doc) => Attribute::Doc(doc),
+            Attribute::Fixity(fixity) => Attribute::Fixity(fixity),
+            Attribute::Derive(ids) => Attribute::Derive(ids),
             Attribute::Specialize(id, tys) => Attribute::Specialize(id, tys.map_snd(f)),
+            Attribute::Custom(id, tokens) => Attribute::Custom(id, tokens),
         }
     }
 }
