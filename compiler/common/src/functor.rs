de28d0bbb869e441d5786919b8428bb832a43f88
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-19 11:51:08 -0500

    renamed `Func::Fresh` to `Func::New` and added some utilities

--- compiler/common/src/functor.rs
+++ compiler/common/src/functor.rs
@@ -1,4 +1,6 @@
-pub use std::collections::{HashMap as Map, HashSet as Set, VecDeque as Deque};
+use std::collections::{BTreeSet, BinaryHeap, LinkedList};
+
+use crate::{either::Either, iter::link::Link, iter::sets::Set, Deque, Map};
 
 /// Defunctionalization type.
 ///
@@ -13,7 +15,7 @@ pub use std::collections::{HashMap as Map, HashSet as Set, VecDeque as Deque};
 /// The ergonomics aren't as *clean* as passing in a simple closure --
 /// but it works ;)
 pub enum Func<'a, F> {
-    Fresh(F),
+    New(F),
     Cont(&'a mut Func<'a, F>),
 }
 
@@ -23,8 +25,58 @@ impl<'a, F> Func<'a, F> {
         F: FnMut(A) -> B,
     {
         match self {
-            Func::Fresh(f) => f(arg),
+            Func::New(f) => f(arg),
             Func::Cont(f) => f.apply(arg),
+        }
+    }
+
+    pub fn call<A, B>(self, arg: A) -> B
+    where
+        F: FnMut(A) -> B,
+    {
+        match self {
+            Func::New(mut f) => f(arg),
+            Func::Cont(f) => f.apply(arg),
+        }
+    }
+
+    pub fn from_closure<A, B>(f: F) -> Self
+    where
+        F: FnMut(A) -> B,
+    {
+        Func::New(f)
+    }
+
+    pub fn identity<X>() -> Func<'a, impl FnMut(X) -> X> {
+        Func::New(|x: X| x)
+    }
+}
+
+impl<F, A, B> From<(F, [(A, B); 0])> for Func<'_, F>
+where
+    F: FnMut(A) -> B,
+{
+    fn from((f, _): (F, [(A, B); 0])) -> Self {
+        Func::New(f)
+    }
+}
+
+impl<F> std::ops::Deref for Func<'_, F> {
+    type Target = F;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Func::New(f) => f,
+            Func::Cont(f) => &**f,
+        }
+    }
+}
+
+impl<F> std::ops::DerefMut for Func<'_, F> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        match self {
+            Func::New(f) => f,
+            Func::Cont(f) => *f,
         }
     }
 }
@@ -35,6 +87,15 @@ pub trait Functor<A, B> {
     fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
     where
         F: FnMut(A) -> B;
+
+    fn mapped<F>(self, f: F) -> Self::Wrapper
+    where
+        Self: Sized,
+        F: FnMut(A) -> B,
+    {
+        let mut f = Func::New(f);
+        self.fmap(&mut f)
+    }
 }
 
 impl<A, B> Functor<A, B> for Option<A> {
@@ -67,6 +128,132 @@ impl<A, B> Functor<A, B> for Box<A> {
         F: FnMut(A) -> B,
     {
         Box::new(f.apply(*self))
+    }
+}
+
+impl<A, B> Functor<A, B> for Deque<A> {
+    type Wrapper = Deque<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        self.into_iter().map(|a| f.apply(a)).collect()
+    }
+}
+
+impl<A, B> Functor<A, B> for Set<A>
+where
+    A: Eq + std::hash::Hash,
+    B: Eq + std::hash::Hash,
+{
+    type Wrapper = Set<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        self.into_iter().map(|a| f.apply(a)).collect()
+    }
+}
+
+impl<A, B> Functor<A, B> for std::collections::hash_set::HashSet<A>
+where
+    A: Eq + std::hash::Hash,
+    B: Eq + std::hash::Hash,
+{
+    type Wrapper = std::collections::hash_set::HashSet<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        self.into_iter().map(|a| f.apply(a)).collect()
+    }
+}
+
+impl<A, B> Functor<A, B> for BTreeSet<A>
+where
+    A: Ord,
+    B: Ord,
+{
+    type Wrapper = BTreeSet<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        self.into_iter().map(|a| f.apply(a)).collect()
+    }
+}
+
+impl<A, B> Functor<A, B> for BinaryHeap<A>
+where
+    A: Ord,
+    B: Ord,
+{
+    type Wrapper = BinaryHeap<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        self.into_iter().map(|a| f.apply(a)).collect()
+    }
+}
+
+impl<A, B, C> Functor<A, C> for Either<A, B> {
+    type Wrapper = Either<C, B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> C,
+    {
+        match self {
+            Either::Left(a) => Either::Left(f.apply(a)),
+            Either::Right(b) => Either::Right(b),
+        }
+    }
+}
+
+impl<A, B> Functor<A, B> for Link<A> {
+    type Wrapper = Link<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        let (a, b) = self.parts();
+        Link::new(f.apply(a), f.apply(b))
+    }
+}
+
+impl<A, B> Functor<A, B> for LinkedList<A> {
+    type Wrapper = LinkedList<B>;
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        self.into_iter().map(|a| f.apply(a)).collect()
+    }
+}
+
+impl<A, B, const N: usize> Functor<A, B> for [A; N] {
+    type Wrapper = [B; N];
+
+    fn fmap<F>(self, f: &mut Func<'_, F>) -> Self::Wrapper
+    where
+        F: FnMut(A) -> B,
+    {
+        use std::mem;
+        let mut arr: [B; N] = unsafe { mem::zeroed() };
+        let mut i = 0;
+        for a in self {
+            arr[i] = f.apply(a);
+            i += 1;
+        }
+        arr
     }
 }
 
@@ -257,6 +444,34 @@ impl<X, E, U> MapSnd<E, U> for Result<X, E> {
     }
 }
 
+impl<A, B, U> MapFst<A, U> for Either<A, B> {
+    type WrapFst = Either<U, B>;
+
+    fn map_fst<F>(self, f: &mut Func<'_, F>) -> Self::WrapFst
+    where
+        F: FnMut(A) -> U,
+    {
+        match self {
+            Either::Left(a) => Either::Left(f.apply(a)),
+            Either::Right(b) => Either::Right(b),
+        }
+    }
+}
+
+impl<A, B, U> MapSnd<B, U> for Either<A, B> {
+    type WrapSnd = Either<A, U>;
+
+    fn map_snd<F>(self, f: &mut Func<'_, F>) -> Self::WrapSnd
+    where
+        F: FnMut(B) -> U,
+    {
+        match self {
+            Either::Left(a) => Either::Left(a),
+            Either::Right(b) => Either::Right(f.apply(b)),
+        }
+    }
+}
+
 impl<K, V, U> MapFst<K, U> for std::collections::HashMap<K, V>
 where
     K: Eq + std::hash::Hash,
@@ -301,143 +516,236 @@ where
     }
 }
 
-// maybe delete later lol
-// pub trait Mappable<X> {
-//     type M<A>: Mappable<A>;
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y;
-// }
+impl<K, V, U> MapFst<K, U> for Map<K, V>
+where
+    K: Eq + std::hash::Hash,
+    U: Eq + std::hash::Hash,
+{
+    type WrapFst = Map<U, V>;
 
-// impl<X> Mappable<X> for Vec<X> {
-//     type M<Y> = Vec<Y>;
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         self.into_iter().map(f).collect()
-//     }
-// }
+    fn map_fst<F>(self, f: &mut Func<'_, F>) -> Self::WrapFst
+    where
+        F: FnMut(K) -> U,
+    {
+        self.into_iter().map(|(k, v)| (f.apply(k), v)).collect()
+    }
+}
 
-// impl<X> Mappable<X> for Deque<X> {
-//     type M<Y> = Deque<Y>;
+impl<K, V, U> MapSnd<V, U> for Map<K, V>
+where
+    K: Eq + std::hash::Hash,
+{
+    type WrapSnd = Map<K, U>;
 
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         self.into_iter().map(f).collect()
-//     }
-// }
+    fn map_snd<F>(self, f: &mut Func<'_, F>) -> Self::WrapSnd
+    where
+        F: FnMut(V) -> U,
+    {
+        self.into_iter().map(|(k, v)| (k, f.apply(v))).collect()
+    }
+}
 
-// impl<K: Eq + std::hash::Hash, X> Mappable<X> for Map<K, X> {
-//     type M<A> = Map<K, A>;
+impl<K, U> MapFst<K, U> for Set<K>
+where
+    K: Eq + std::hash::Hash,
+    U: Eq + std::hash::Hash,
+{
+    type WrapFst = Set<U>;
 
-//     fn fmap<F, Y>(self, mut f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         self.into_iter().map(|(k, x)| (k, f(x))).collect()
-//     }
-// }
+    fn map_fst<F>(self, f: &mut Func<'_, F>) -> Self::WrapFst
+    where
+        F: FnMut(K) -> U,
+    {
+        self.into_iter().map(|k| f.apply(k)).collect()
+    }
+}
 
-// impl<'x, X> Mappable<&'x X> for &'x [X] {
-//     type M<A> = Vec<A>;
+pub type Wrap1<X, A, B> = <X as MapFst<A, B>>::WrapFst;
+pub type Wrap2<X, A, B> = <X as MapSnd<A, B>>::WrapSnd;
+pub type BiWrap<X, A, B, C, D> = <<X as MapFst<A, B>>::WrapFst as MapSnd<C, D>>::WrapSnd;
 
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(&'x X) -> Y,
-//     {
-//         self.into_iter().map(f).collect()
-//     }
-// }
+pub trait Bifunctor<A, B, C, D>: MapFst<A, B> + MapSnd<C, D>
+where
+    // <Self as MapFst<A, B>>::WrapFst: MapSnd<C, D>,
+    Wrap1<Self, A, B>: MapSnd<C, D>,
+{
+    fn bimap<F: FnMut(A) -> B, G: FnMut(C) -> D>(
+        self,
+        f: &mut Func<'_, F>,
+        g: &mut Func<'_, G>,
+    ) -> BiWrap<Self, A, B, C, D>
+    where
+        Self: Sized,
+    {
+        self.map_fst(f).map_snd(g)
+    }
+}
 
-// impl<X> Mappable<X> for Option<X> {
-//     type M<A> = Option<A>;
+impl<X, A, B, C, D> Bifunctor<A, B, C, D> for X
+where
+    X: MapFst<A, B> + MapSnd<C, D>,
+    Wrap1<Self, A, B>: MapSnd<C, D>,
+{
+}
 
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         self.map(f)
-//     }
-// }
+/// A simple trait allowing for reverse composition using method
+/// chaining that can be implemented for all types. Note that the
+/// by-value method requires the receiver to be `Sized`.
+pub trait PipeF {
+    #[inline]
+    fn pipe<X>(self, f: impl FnOnce(Self) -> X) -> X
+    where
+        Self: Sized,
+    {
+        f(self)
+    }
 
-// impl<X, E> Mappable<X> for Result<X, E> {
-//     type M<A> = Result<A, E>;
+    #[inline]
+    fn pipe_ref<X>(&self, f: impl FnOnce(&Self) -> X) -> X {
+        f(self)
+    }
 
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         self.map(f)
-//     }
-// }
+    #[inline]
+    fn pipe_mut<X>(&mut self, f: impl FnOnce(&mut Self) -> X) -> X {
+        f(self)
+    }
 
-// impl<X, const N: usize> Mappable<X> for [X; N] {
-//     type M<A> = [A; N];
+    #[inline]
+    fn pipe2<X, Y>(self, f: impl FnOnce(Self, X) -> Y + 'static) -> Box<dyn FnOnce(X) -> Y>
+    where
+        Self: Sized + 'static,
+    {
+        Box::new(move |x: X| f(self, x))
+    }
 
-//     fn fmap<F, Y>(self, f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         let mut array: [Y; N] = unsafe { std::mem::zeroed() };
-//         for (i, y) in self.into_iter().map(f).enumerate() {
-//             array[i] = y;
-//         }
-//         array
-//     }
-// }
+    #[inline]
+    fn flip<'a, X, Y, F>(&'a self, f: F) -> Box<dyn FnOnce(X) -> Y + 'a>
+    where
+        Self: Sized + 'a,
+        F: FnOnce(X, &Self) -> Y + 'a,
+    {
+        Box::new(move |x: X| f(x, self))
+    }
 
-// impl<X> Mappable<X> for Box<X> {
-//     type M<A> = Box<A>;
+    fn flp<F, X>(&self, f: F) -> F
+    where
+        for<'a> F: FnOnce(X, &Self),
+    {
+        f
+    }
+}
 
-//     fn fmap<F, Y>(self, mut f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         Box::new(f(*self))
-//     }
-// }
+impl<T> PipeF for T {}
 
-// impl<U, V, X> Mappable<X> for (U, V)
-// where
-//     U: Mappable<X>,
-//     V: Mappable<X>,
-// {
-//     type M<A> = (U::M<A>, V::M<A>);
+pub fn flip<'a, F, X, Y, Z>(f: F) -> Func<'a, impl FnOnce(Y, X) -> Z>
+where
+    F: 'a + FnOnce(X, Y) -> Z,
+{
+    Func::New(|y: Y, x: X| f(x, y))
+}
 
-//     fn fmap<F, Y>(self, mut f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         let (u, v) = self;
-//         (u.fmap(|x| f(x)), v.fmap(|x| f(x)))
-//     }
-// }
+pub fn identity<X>(x: X) -> X {
+    x
+}
 
-// impl<X> Mappable<X> for () {
-//     type M<A> = ();
+pub fn uncurry<'a, G, H, X, Y, Z>(mut g: G) -> Func<'a, impl FnMut(X, Y) -> Z>
+where
+    G: FnMut(X) -> H,
+    H: FnMut(Y) -> Z,
+{
+    Func::New(move |x: X, y: Y| g(x)(y))
+}
 
-//     fn fmap<F, Y>(self, _f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         ()
-//     }
-// }
+impl<X> Default for Func<'_, fn(X) -> X> {
+    fn default() -> Self {
+        Func::New(identity)
+    }
+}
 
-// impl<X> Mappable<X> for (X,) {
-//     type M<A> = (A,);
+pub struct Curry<F>(F);
+impl<F> Curry<F> {
+    pub fn apply<G, X, Y, Z>(self, x: X) -> G
+    where
+        F: FnOnce(X) -> G,
+        G: FnOnce(Y) -> Z,
+    {
+        (self.0)(x)
+    }
 
-//     fn fmap<F, Y>(self, mut f: F) -> Self::M<Y>
-//     where
-//         F: FnMut(X) -> Y,
-//     {
-//         let (x,) = self;
-//         (f(x),)
-//     }
-// }
+    pub fn apply_mut<G, X, Y, Z>(self, x: X) -> G
+    where
+        F: FnMut(X) -> G,
+        G: FnMut(Y) -> Z,
+    {
+        let Curry(mut f) = self;
+        f(x)
+    }
+
+    pub fn uncurry<G, X, Y, Z>(self) -> impl FnOnce(X, Y) -> Z
+    where
+        F: FnOnce(X) -> G,
+        G: FnOnce(Y) -> Z,
+    {
+        move |x: X, y: Y| self.apply(x)(y)
+    }
+
+    pub fn uncurry_mut<G, X, Y, Z>(self) -> impl FnMut(X, Y) -> Z
+    where
+        F: FnMut(X) -> G,
+        G: FnMut(Y) -> Z,
+    {
+        let Curry(mut f) = self;
+        move |x: X, y: Y| f(x)(y)
+    }
+}
+
+impl<F: Clone> Clone for Curry<F> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<F: Copy> Copy for Curry<F> {}
+
+impl<F> std::ops::Deref for Curry<F> {
+    type Target = F;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<F> std::ops::DerefMut for Curry<F> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<F> AsRef<F> for Curry<F> {
+    fn as_ref(&self) -> &F {
+        &self.0
+    }
+}
+
+impl<F> AsMut<F> for Curry<F> {
+    fn as_mut(&mut self) -> &mut F {
+        &mut self.0
+    }
+}
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_func() {
+        let f = Func::New(|x| x);
+        let y = f(8);
+        assert!(y == 8);
+
+        // let f = |x, y| (x, y);
+        // let g = flip(f);
+        // let g = g(1, 'z');
+        // assert_eq!(g, ('z', 1))
+    }
+}
